2691e81787bb5ded5992f29e2050e585a9b2ef7e
nutmeggs <143857410+nutmeggs@users.noreply.github.com>
2024-05-21 14:23:47 +1000

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,37 @@
+import re
+from django.contrib import admin
+from django.urls import path
+from django.urls import include, re_path
+import app.views
+from app.views import studenthome
+
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+
+    re_path(r'^$', app.views.index, name = 'index'),
+
+    re_path(r'^home$', app.views.index, name = 'home'),
+
+    path('studenthome/', studenthome, name='student_home'),
+
+    path('dashboard/', app.views.add_item, name='dashboard'),
+]
+
+
+
+
+
+
+
+
+
+#from django.urls import path, include
+
+#from .views import studenthome
+
+#urlspattern = [
+    #path("", studenthome, name='studenthome'),
+
+#]
