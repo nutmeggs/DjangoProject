2620333d1e52c190d7e19ef8da05bcae5744df00
nutmeggs <143857410+nutmeggs@users.noreply.github.com>
2024-05-21 14:22:53 +1000

    Create models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlite3 import DatabaseError
+from django.db import models
+from django.utils import timezone
+
+class Asset(models.Model):
+   Type = models.CharField(max_length=50, blank=True, null=True)
+   Name = models.CharField(max_length=50, blank=True, null=True)
+   Email = models.CharField(max_length=50, blank=True, null=True)
+   Hire_Date = models.DateTimeField (default = timezone.now)
+
+   def __str_(self):
+       return self.code + '-' + self.Type
