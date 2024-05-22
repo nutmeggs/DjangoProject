b8cf1456594a1cab56ffad7905fc8bb60503d1db
nutmeggs <143857410+nutmeggs@users.noreply.github.com>
2024-05-22 20:56:50 +1000

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from sqlite3 import DatabaseError
 from django.db import models
+from sqlite3 import DatabaseError
 from django.utils import timezone
 
 class Asset(models.Model):
@@ -8,11 +8,14 @@ class Asset(models.Model):
    Email = models.CharField(max_length=50, blank=True, null=True)
    Hire_Date = models.DateTimeField (default = timezone.now)
 
-
-
    def __str__(self):
        return f"{self.Name} - {self.Type}"
 
-   
-    def __str__(self):
-       return self.code + '-' + self.Type
+
+# Create your models here.
+
+class teacher(models.Model):
+
+    Name = models.CharField(max_length=25)
+
+    Area = models.CharField(max_length=30)
